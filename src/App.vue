ed4774158efa9930f1e9c349cb8bd50f0fc6b874
XiNGRZ <hi@xingrz.me>
2022-05-08 02:27:47 +0800

    Better layout

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,49 @@
 <template>
-  <a-layout>
-    <a-layout-content :style="{ padding: '0 50px', marginTop: '24px' }">
-      <a-alert type="info" show-icon :class="$style.subtitle">
-        <template #message>
-          在找字幕编辑器吗？它已经被移到了 <b><a href="https://subtitle.xingrz.me/">subtitle.xingrz.me</a></b>。
-        </template>
-      </a-alert>
+  <a-input v-model:value="keyword" placeholder="输入官方集数、拆分集数或标题关键词搜索" autofocus :class="$style.entry" />
 
-      <a-input v-model:value="keyword" placeholder="输入官方集数、拆分集数或标题关键词搜索" autofocus :class="$style.entry" />
+  <a-table :columns="columns" :data-source="filtered" row-key="num_jp" :expand-row-by-click="true"
+    :class="$style.table">
+    <template #bodyCell="{ column, record }">
+      <template v-if="column.dataIndex == 'aired_at'">
+        {{ record.aired_at }}
+      </template>
+      <template v-else-if="column.dataIndex == 'num_jp'">
+        <div :class="{ [$style.highlight]: String(record.num_jp) == keyword }">{{ record.num_jp }}</div>
+        <div v-if="record.num_cn.length > 1" :class="$style.sp">
+          <a-tag color="orange" v-if="record.num_cn.length == 2">1HSP</a-tag>
+          <a-tag color="orange" v-else-if="record.num_cn.length == 3">1.5HSP</a-tag>
+          <a-tag color="red" v-else-if="record.num_cn.length == 4">2HSP</a-tag>
+          <a-tag color="purple" v-else-if="record.num_cn.length == 5">2.5HSP</a-tag>
+        </div>
+      </template>
+      <template v-else-if="column.dataIndex == 'num_cn'">
+        <template v-for="num in record.num_cn" :key="num">
+          <div :class="{ [$style.highlight]: String(num) == keyword }">{{ num }}</div>
+        </template>
+      </template>
+      <template v-else-if="column.dataIndex == 'manga'">
+        <div v-for="(vol, i) in record.manga" :key="vol" :class="$style.manga">
+          <a-tag :color="getMangaColor(vol)">{{ vol }}</a-tag>
+        </div>
+      </template>
+      <template v-else-if="column.dataIndex == 'title'">
+        <div v-html="highlight(record.title_jp)" lang="ja" />
+        <div v-html="highlight(record.title_cn)" />
+      </template>
+    </template>
+    <template #expandedRowRender="{ record }">
+      <h4>相关链接</h4>
+      <ul :class="$style.links">
+        <li><a :href="`https://www.conanpedia.com/TV${record.num_jp}`" target="_blank" noreferer>资料：柯南百科</a></li>
+      </ul>
+    </template>
+  </a-table>
 
-      <a-table :columns="columns" :data-source="filtered" row-key="num_jp" :expand-row-by-click="true"
-        :class="$style.table">
-        <template #bodyCell="{ column, record }">
-          <template v-if="column.dataIndex == 'aired_at'">
-            {{ record.aired_at }}
-          </template>
-          <template v-else-if="column.dataIndex == 'num_jp'">
-            <div :class="{ [$style.highlight]: String(record.num_jp) == keyword }">{{ record.num_jp }}</div>
-            <div v-if="record.num_cn.length > 1" :class="$style.sp">
-              <a-tag color="orange" v-if="record.num_cn.length == 2">1HSP</a-tag>
-              <a-tag color="orange" v-else-if="record.num_cn.length == 3">1.5HSP</a-tag>
-              <a-tag color="red" v-else-if="record.num_cn.length == 4">2HSP</a-tag>
-              <a-tag color="purple" v-else-if="record.num_cn.length == 5">2.5HSP</a-tag>
-            </div>
-          </template>
-          <template v-else-if="column.dataIndex == 'num_cn'">
-            <template v-for="num in record.num_cn" :key="num">
-              <div :class="{ [$style.highlight]: String(num) == keyword }">{{ num }}</div>
-            </template>
-          </template>
-          <template v-else-if="column.dataIndex == 'manga'">
-            <div v-for="(vol, i) in record.manga" :key="vol" :class="$style.manga">
-              <a-tag :color="getMangaColor(vol)">{{ vol }}</a-tag>
-            </div>
-          </template>
-          <template v-else-if="column.dataIndex == 'title'">
-            <div v-html="highlight(record.title_jp)" lang="ja" />
-            <div v-html="highlight(record.title_cn)" />
-          </template>
-        </template>
-        <template #expandedRowRender="{ record }">
-          <h4>相关链接</h4>
-          <ul :class="$style.links">
-            <li><a :href="`https://www.conanpedia.com/TV${record.num_jp}`" target="_blank" noreferer>资料：柯南百科</a></li>
-          </ul>
-        </template>
-      </a-table>
-    </a-layout-content>
-  </a-layout>
+  <a-alert type="info" show-icon :class="$style.subtitle">
+    <template #message>
+      在找字幕编辑器吗？它已经被移到了 <b><a href="https://subtitle.xingrz.me/">subtitle.xingrz.me</a></b>。
+    </template>
+  </a-alert>
 </template>
 
 <script lang="ts" setup>
@@ -130,12 +126,23 @@ function getMangaColor(vol: string): string {
 </script>
 
 <style lang="scss" module>
+:global(html, body) {
+  margin: 0;
+  padding: 0;
+}
+
 :global(body) {
   background: #f0f2f5;
 }
 
+:global(#app) {
+  padding: 32px 50px;
+  margin: 0 auto;
+  max-width: 1200px;
+}
+
 .subtitle {
-  margin-bottom: 24px;
+  margin-top: 24px;
 }
 
 .entry {
